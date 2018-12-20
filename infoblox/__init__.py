e4380bdcfd493f4abe77db03075cf86fdd592324
lewoudar <lewoudar@gmail.com>
2018-12-20 22:07:59 +0100

    fix(infoblox/__init__.py): add __version__ to explicit imports

--- infoblox/__init__.py
+++ infoblox/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
 
-__all__ = ['Client', 'Resource']
+__all__ = ['__version__', 'Client', 'Resource']
 
 from infoblox.client import Client
 from infoblox.resource import Resource
